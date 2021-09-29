33114d5d1c781dc257a73e444cecf3560aec552e
Eric <anccaj@orange.fr>
2021-09-29 09:42:01 +0200

    ajout script et css bouton top

---
+++ scripts/scripts.js
@@ -0,0 +1,30 @@
+
+/* get a reference to our predefined button*/
+var scrollToTopBtn = document.querySelector(".scrollToTopBtn");
+console.log(scrollToTopBtn);
+if(scrollToTopBtn){
+  document.addEventListener("scroll", handleScroll);
+ function handleScroll() {
+  var scrollableHeight = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+  var GOLDEN_RATIO = 0.5;
+
+  if ((document.documentElement.scrollTop / scrollableHeight ) > GOLDEN_RATIO) {
+    //show button
+    if(!scrollToTopBtn.classList.contains("showScrollBtn"))
+    scrollToTopBtn.classList.add("showScrollBtn")
+  } else {
+    //hide button
+    if(scrollToTopBtn.classList.contains("showScrollBtn"))
+    scrollToTopBtn.classList.remove("showScrollBtn")
+  }
+}
+
+scrollToTopBtn.addEventListener("click", scrollToTop);
+
+function scrollToTop() {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth"
+  });
+}
+}
